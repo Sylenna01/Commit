92e99423a157fd50c60bb73e764aadb7dea20e21
Sylenna01 <ifeoluwav21@gmail.com>
2024-10-11 14:18:18 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// arithmetic opetator
+
+let x = 5;
+let y = 2;
+
+let z = x * y;
+let sum = x + y;
+console.log(sum);
+console.log(z);
+
+
+// comparison operator
+
+
+let num1 = 1;
+let num2 = 2;
+
+if (num1 > num2) {
+    console.log(true)
+} else {
+    console.log(false)
+}
+
+
+// let numone = 1;
+// let numtwo = 2;
+
+
+//modulo 
+
+let m = 15;
+let n = 4;
+
+let division= m % n;
+console.log(division);
